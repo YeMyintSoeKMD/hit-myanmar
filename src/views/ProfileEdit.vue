6100c8873cc4c0e7e4e25bfc38571e4276b51181
htetnandarbo <htetnandarbo8@gmail.com>
2024-04-13 14:55:35 +0630

    hndb

--- src/views/ProfileEdit.vue
+++ src/views/ProfileEdit.vue
@@ -16,17 +16,29 @@
 
             <div class="flex flex-col mb-4">
                 <label class="text-lg font-medium mb-2">Image Preview</label>
-                <div class="border">
-                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                        class="w-60 py-5 m-auto" />
+                <div class="border py-5">
+                    <div v-show="!imageUrl">
+                        <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                            class="w-60 m-auto" />
+                    </div>
+                    <div v-show="imageUrl">
+                        <img :src="imageUrl" class="w-60 m-auto" />
+                    </div>
+                    <input type="file" class="hidden" ref="fileInput" @change="handleFileSelect">
+                    <div @click="triggerFileInput" class="cursor-pointer flex justify-center">
+                        <p
+                            class="w-32 py-2 px-4 rounded-md text-sm font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100 mt-5">
+                            Upload Image</p>
+                    </div>
                 </div>
+
             </div>
 
-            <div class="flex flex-col mb-4">
+            <!-- <div class="flex flex-col mb-4">
                 <label class="text-lg font-medium mb-2">Change Image</label>
                 <input type="file"
                     class="file-input file-input-bordered w-full file:bg-white file:text-gray-700 file:border-0 file:border-r file:border-inherit hover:file:bg-violet-100" />
-            </div>
+            </div> -->
 
             <div class="mt-8">
                 <button class="btn btn-info text-white text-lg px-10">Save</button>
@@ -36,7 +48,29 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const fileInput = ref();
+const imageUrl = ref();
+
+const triggerFileInput = () => {
+    if (fileInput.value) {
+        fileInput.value.click();
+    }
+};
+
+const handleFileSelect = (event) => {
+
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            imageUrl.value = e.target.result;
+        };
+        reader.readAsDataURL(selectedFile);
+    }
+};
 </script>
 
 <style lang="scss" scoped></style>
