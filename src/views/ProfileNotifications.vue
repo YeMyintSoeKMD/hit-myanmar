00a0566580ee58dcd361580b97710c90fdefc2d7
htetnandarbo <htetnandarbo8@gmail.com>
2024-05-15 15:37:50 +0630

    nofi modal

--- src/views/ProfileNotifications.vue
+++ src/views/ProfileNotifications.vue
@@ -34,7 +34,7 @@
             </div>
         </div>
 
-        <div class="flex bg-sky-10 ps-5 py-4 mb-1">
+        <div class="flex bg-sky-10 ps-5 py-4 mb-1" onclick="my_modal_2.showModal()">
             <div class="w-10">
                 <div class="bg-orange-50 w-10 h-10 rounded-full flex justify-center items-center">
                     <img src="/images/notification1.png" class="w-8 h-8" />
@@ -109,6 +109,34 @@
                 <span class="text-sm text-orange-600">1 hour ago</span>
             </div>
         </div>
+
+        <div>
+            <dialog id="my_modal_2" class="modal">
+                <div class="modal-box md:w-9/12 w-11/12 max-w-5xl">
+                    <h3 class="font-bold text-lg mb-1">You have 1 new message</h3>
+                    <div class="flex py-4 mb-1">
+            <div class="w-10">
+                <div class="bg-orange-50 w-10 h-10 rounded-full flex justify-center items-center">
+                    <img src="/images/notification1.png" class="w-8 h-8" />
+                </div>
+            </div>
+            <div class="ms-5">
+                <div class="flex items-start">
+                    <p class="mb-1 basis-11/12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, sit Lorem
+                        ipsum dolor sit
+                        amet, consectetur adipisicing elit Lorem ipsum dolor sit netur esse neque lorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consectetur enim labore, aliquid deleniti cumque blanditiis porro quasi laudantium iste, animi magnam minima. Deleniti temporibus excepturi quia delectus nesciunt maiores?</p>
+                </div>
+                <span class="text-sm text-orange-600">1 hour ago</span>
+            </div>
+        </div>
+                </div>
+                <form method="dialog" class="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+        </div>
+
+
 
 
 
