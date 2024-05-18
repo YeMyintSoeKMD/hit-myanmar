4ad6da1bd5abcaa2a096e1969e1f580e8fb93070
htetnandarbo <htetnandarbo8@gmail.com>
2024-05-18 22:45:32 +0630

    gallery edit

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,107 +1,203 @@
 <template>
   <div class="container mx-auto px-8 2xl:px-40 pt-40">
-    <div>
+    <div class="pb-20">
       <div class="pb-28">
-        <div class="box-container">
-          <div class="box">
 
-            <div class="title">
-              <span class="block"></span>
-              <h1 class="space-grotesk-600">Welcome To Our<span></span></h1>
-            </div>
-
-            <div class="role">
-              <div class="block"></div>
-              <p class="space-grotesk-600">Campus Gallery</p>
-            </div>
-
-          </div>
+        <div class="title">
+          <h1 class="space-grotesk-600 text-gallery2 flex items-end">Welcome To Our<span
+              class="w-2 h-2 bg-info block rounded-full mb-7 ms-3"></span>
+          </h1>
+          <p class="space-grotesk-600 text-gallery text-orange-500 -mt-2">Campus Gallery</p>
         </div>
 
-        <p class="w-5/12 text-justify mt-14 text-gray-600 text-sm leading-6">
+
+        <p class="w-5/12 text-justify mt-8 text-gray-600 text-sm leading-6">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates earum consectetur asperiores at cum
           dolores
           harum facilis nulla, animi labore cupiditate ratione dignissimos
         </p>
       </div>
-
-
       <div class="mt-14 flex justify-end gap-5 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
         <div class="ml-auto flex-none space-y-4 sm:ml-0 lg:order-last xl:order-none">
           <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
-            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-          </div>
-          <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
+            <img src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""
+              class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
         </div>
         <div class="mr-auto flex-none space-y-4 sm:mr-0 -mt-12">
           <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
-            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-          </div>
-          <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-72" />
+            <img src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""
+              class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-68" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
         </div>
         <div class="flex-none space-y-4 -mt-20">
           <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-80" />
-            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-          </div>
-          <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
+            <img src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""
+              class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-80" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
         </div>
-        <div class="flex-none space-y-4 -mt-90">
+        <div class="flex-none space-y-4 -mt-100">
           <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-56" />
+            <img src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""
+              class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-100 h-64" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
           <div class="relative">
-            <img
-              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-100" />
+            <img src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""
+              class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-100 h-96" />
             <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
           </div>
 
         </div>
-        <!-- <div class="flex-none space-y-4 -mt-90">
-        <div class="relative">
-          <img
-            src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-            alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-96" />
-          <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-        </div>
-        <div class="relative">
-          <img
-            src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-            alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-96" />
-          <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+      </div>
+    </div>
+
+    <div class="py-20">
+      <div class="grid grid-cols-2">
+        <div class="w-56">
+          <h1 class="text-3xl leading-normal space-grotesk-500">A Photo Gallery of The Lorem</h1>
+          <div class="divider divider-info before:h-0.5 after:h-0.5 m-1 w-64"></div>
+          <p class="text-gray-500 text-sm mt-2">Lorem ipsum dolor, sit amet consectetur Lorem, ipsum dolor.</p>
         </div>
 
-      </div> -->
+
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="py-20">
+      <div class="grid grid-cols-2">
+        <div class="w-56">
+          <h1 class="text-3xl leading-normal space-grotesk-500">A Photo Gallery of The Lorem</h1>
+          <div class="divider divider-info before:h-0.5 after:h-0.5 m-1 w-64"></div>
+          <p class="text-gray-500 text-sm mt-2">Lorem ipsum dolor, sit amet consectetur Lorem, ipsum dolor.</p>
+        </div>
+
+
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
+            </div>
+            <div>
+              <img class="h-auto max-w-full rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
+
 
 </template>
 
@@ -112,258 +208,17 @@
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap');
 
-.box-container {
-
-  .box {
-    width: 800px;
-    position: relative;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-
-    .title {
-      width: 100%;
-      position: relative;
-      display: flex;
-      align-items: center;
-      height: 50px;
-
-      .block {
-        width: 0%;
-        height: inherit;
-        background: #30a7d6;
-        position: absolute;
-        animation: mainBlock 2s cubic-bezier(.74, .06, .4, .92) forwards;
-        display: flex;
-      }
-
-      h1 {
-        color: #000;
-        font-size: 70px;
-        letter-spacing: 4px;
-        -webkit-animation: mainFadeIn 2s forwards;
-        -o-animation: mainFadeIn 2s forwards;
-        animation: mainFadeIn 2s forwards;
-        animation-delay: 1.6s;
-        opacity: 0;
-        display: flex;
-        align-items: baseline;
-        position: relative;
-
-        span {
-          width: 0px;
-          height: 0px;
-          -webkit-border-radius: 50%;
-          -moz-border-radius: 50%;
-          border-radius: 50%;
-
-          background: #30a7d6;
-          -webkit-animation: load 0.6s cubic-bezier(.74, .06, .4, .92) forwards;
-          animation: popIn 0.8s cubic-bezier(.74, .06, .4, .92) forwards;
-          animation-delay: 2s;
-          margin-left: 5px;
-          margin-top: -10px;
-          /* position: absolute; */
-          bottom: 13px;
-          right: -12px;
-
-        }
-      }
-    }
-
-    .role {
-      width: 100%;
-      position: relative;
-      display: flex;
-      align-items: center;
-      height: 30px;
-      margin-top: 60px;
-
-      .block {
-        width: 0%;
-        height: inherit;
-        background: #f77f2f;
-        position: absolute;
-        animation: secBlock 2s cubic-bezier(.74, .06, .4, .92) forwards;
-        animation-delay: 2s;
-        display: flex;
-      }
-
-      p {
-        animation: secFadeIn 2s forwards;
-        animation-delay: 3.2s;
-        opacity: 0;
-        color: #e96f1e;
-        font-size: 80px;
-      }
-    }
-  }
-}
-
-@keyframes mainBlock {
-  0% {
-    width: 0%;
-    left: 0;
-
-  }
-
-  50% {
-    width: 100%;
-    left: 0;
-
-  }
-
-  100% {
-    width: 0;
-    left: 100%;
-  }
-}
-
-@keyframes secBlock {
-  0% {
-    width: 0%;
-    left: 0;
-
-  }
-
-  50% {
-    width: 100%;
-    left: 0;
-
-  }
-
-  100% {
-    width: 0;
-    left: 100%;
-  }
-}
-
-@keyframes mainFadeIn {
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 1;
-  }
-}
-
-
-@keyframes popIn {
-  0% {
-    width: 0px;
-    height: 0px;
-    background: #30a7d6;
-    border: 0px solid #ddd;
-    opacity: 0;
-  }
-
-  50% {
-    width: 10px;
-    height: 10px;
-    background: #30a7d6;
-    opacity: 1;
-    bottom: 45px;
-  }
-
-  65% {
-    width: 7px;
-    height: 7px;
-    bottom: 0px;
-    width: 15px
-  }
-
-  80% {
-    width: 10px;
-    height: 10px;
-    bottom: 20px
-  }
-
-  100% {
-    width: 7px;
-    height: 7px;
-    background: #30a7d6;
-    border: 0px solid #222;
-    bottom: 13px;
-
-  }
-}
-
-@keyframes secFadeIn {
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 1;
-  }
-}
-
-footer {
-  width: 350px;
-  height: 80px;
-  background: #ffb510;
-  position: absolute;
-  right: 0;
-  bottom: -80px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: top 0.8s forwards;
-  animation-delay: 4s;
-
-  span {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 12px;
-    color: #232323;
-    font-family: 'Poppins';
-
-    i {
-      margin-right: 25px;
-      font-size: 22px;
-      color: #232323;
-      animation: icon 2s forwards;
-      animation-delay: 4s;
-      opacity: 0;
-    }
-  }
-}
-
-@keyframes top {
-  0% {
-    opacity: 0;
-    bottom: -80px
-  }
-
-  100% {
-    opacity: 1;
-    bottom: 0px
-  }
-}
-
-@keyframes icon {
-  0% {
-    opacity: 0;
-    transform: scale(0.0);
-  }
-
-  50% {
-    opacity: 1;
-    transform: scale(1.3) rotate(-02deg);
-  }
-
-  100% {
-    opacity: 1;
-    bottom: 0px;
-  }
-}
-
 .space-grotesk-600 {
   font-family: "Space Grotesk", sans-serif;
   font-optical-sizing: auto;
   font-weight: 600;
+  font-style: normal;
+}
+
+.space-grotesk-500 {
+  font-family: "Space Grotesk", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
   font-style: normal;
 }
 </style>
