016c923c6097da69d134bd859939547ae1529c1e
htetnandarbo <htetnandarbo8@gmail.com>
2024-05-18 20:58:33 +0630

    gallery edit

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,105 +1,108 @@
 <template>
-    <div class="container mx-auto px-8 2xl:px-40 pt-40">
-        <div class="pb-28">
-            <div class="box-container">
-                <div class="box">
+  <div class="container mx-auto px-8 2xl:px-40 pt-40">
+    <div>
+      <div class="pb-28">
+        <div class="box-container">
+          <div class="box">
 
-        <div class="title">
-            <span class="block"></span>
-            <h1 class="space-grotesk-600">Welcome To Our<span></span></h1>
-        </div>
-
-        <div class="role">
-            <div class="block"></div>
-            <p class="space-grotesk-600">Campus Gallery</p>
-        </div>
-
-                </div>
+            <div class="title">
+              <span class="block"></span>
+              <h1 class="space-grotesk-600">Welcome To Our<span></span></h1>
             </div>
 
-            <p class="w-1/3 text-justify mt-10 text-gray-600 text-sm leading-6">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates earum consectetur asperiores at cum dolores harum facilis nulla, animi labore cupiditate ratione dignissimos
-            </p>
-        </div>
-
-       
-        <div class="mt-14 flex justify-end gap-4 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
-              <div
-                class="ml-auto flex-none space-y-4 sm:ml-0 lg:order-last xl:order-none">
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg w-40 h-52" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg w-40 h-52" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-              </div>
-              <div class="mr-auto flex-none space-y-4 sm:mr-0 -mt-8">
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg w-40 h-52" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg w-40 h-60" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-              </div>
-              <div class="flex-none space-y-4 -mt-20">
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg w-40 h-72" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg w-40 h-52" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-              </div>
-              <div class="flex-none space-y-4 -mt-90">
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-72 h-40" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-72 h-100" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-               
-              </div>
-              <div class="flex-none space-y-4 -mt-90">
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-96" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-                <div class="relative">
-                  <img
-                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                    alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-96" />
-                  <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-                </div>
-               
-              </div>
+            <div class="role">
+              <div class="block"></div>
+              <p class="space-grotesk-600">Campus Gallery</p>
             </div>
+
           </div>
-    
+        </div>
+
+        <p class="w-5/12 text-justify mt-14 text-gray-600 text-sm leading-6">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates earum consectetur asperiores at cum
+          dolores
+          harum facilis nulla, animi labore cupiditate ratione dignissimos
+        </p>
+      </div>
+
+
+      <div class="mt-14 flex justify-end gap-5 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+        <div class="ml-auto flex-none space-y-4 sm:ml-0 lg:order-last xl:order-none">
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+        </div>
+        <div class="mr-auto flex-none space-y-4 sm:mr-0 -mt-12">
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-72" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+        </div>
+        <div class="flex-none space-y-4 -mt-20">
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-80" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-48 h-60" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+        </div>
+        <div class="flex-none space-y-4 -mt-90">
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-56" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+          <div class="relative">
+            <img
+              src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+              alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-100" />
+            <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+          </div>
+
+        </div>
+        <!-- <div class="flex-none space-y-4 -mt-90">
+        <div class="relative">
+          <img
+            src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+            alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-96" />
+          <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+        </div>
+        <div class="relative">
+          <img
+            src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+            alt="" class="aspect-[2/3] rounded-xl bg-gray-900/5 object-cover shadow-lg w-96 h-96" />
+          <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+        </div>
+
+      </div> -->
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script setup>
@@ -108,10 +111,11 @@
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap');
+
 .box-container {
 
   .box {
-    width: 600px;
+    width: 800px;
     position: relative;
     display: flex;
     justify-content: center;
@@ -135,7 +139,7 @@
 
       h1 {
         color: #000;
-        font-size: 50px;
+        font-size: 70px;
         letter-spacing: 4px;
         -webkit-animation: mainFadeIn 2s forwards;
         -o-animation: mainFadeIn 2s forwards;
@@ -147,7 +151,7 @@
         position: relative;
 
         span {
-          width:0px;
+          width: 0px;
           height: 0px;
           -webkit-border-radius: 50%;
           -moz-border-radius: 50%;
@@ -173,7 +177,7 @@
       display: flex;
       align-items: center;
       height: 30px;
-      margin-top: 30px;
+      margin-top: 60px;
 
       .block {
         width: 0%;
@@ -190,7 +194,7 @@
         animation-delay: 3.2s;
         opacity: 0;
         color: #e96f1e;
-        font-size: 60px;
+        font-size: 80px;
       }
     }
   }
@@ -202,11 +206,13 @@
     left: 0;
 
   }
+
   50% {
     width: 100%;
     left: 0;
 
   }
+
   100% {
     width: 0;
     left: 100%;
@@ -219,11 +225,13 @@
     left: 0;
 
   }
+
   50% {
     width: 100%;
     left: 0;
 
   }
+
   100% {
     width: 0;
     left: 100%;
@@ -234,6 +242,7 @@
   0% {
     opacity: 0;
   }
+
   100% {
     opacity: 1;
   }
@@ -248,6 +257,7 @@
     border: 0px solid #ddd;
     opacity: 0;
   }
+
   50% {
     width: 10px;
     height: 10px;
@@ -255,17 +265,20 @@
     opacity: 1;
     bottom: 45px;
   }
-   65% {
-      width: 7px;
+
+  65% {
+    width: 7px;
     height: 7px;
-      bottom: 0px;
-      width: 15px
-   }
-   80% {
-      width: 10px;
+    bottom: 0px;
+    width: 15px
+  }
+
+  80% {
+    width: 10px;
     height: 10px;
-      bottom: 20px
-   }
+    bottom: 20px
+  }
+
   100% {
     width: 7px;
     height: 7px;
@@ -280,6 +293,7 @@
   0% {
     opacity: 0;
   }
+
   100% {
     opacity: 1;
   }
@@ -295,8 +309,9 @@ footer {
   display: flex;
   justify-content: center;
   align-items: center;
-   animation: top 0.8s forwards;
-   animation-delay: 4s;
+  animation: top 0.8s forwards;
+  animation-delay: 4s;
+
   span {
     display: flex;
     align-items: center;
@@ -319,27 +334,29 @@ footer {
 @keyframes top {
   0% {
     opacity: 0;
-     bottom: -80px
+    bottom: -80px
   }
+
   100% {
     opacity: 1;
-     bottom: 0px
-
+    bottom: 0px
   }
 }
 
 @keyframes icon {
   0% {
     opacity: 0;
-     transform: scale(0.0);
+    transform: scale(0.0);
   }
-   50% {
-      opacity: 1;
-     transform: scale(1.3) rotate(-02deg);
-   }
+
+  50% {
+    opacity: 1;
+    transform: scale(1.3) rotate(-02deg);
+  }
+
   100% {
     opacity: 1;
-     bottom: 0px;
+    bottom: 0px;
   }
 }
 
@@ -349,5 +366,4 @@ footer {
   font-weight: 600;
   font-style: normal;
 }
-
 </style>
