6b0cb1171bdfa556eac7cfa6b48b2e59e71e6a62
YeMyintSoeKMD <yms.yemyintsoe.kmd@gmail.com>
2024-06-06 22:54:44 +0630

    yms

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -2,19 +2,16 @@
   <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-x-4 gap-y-10 mt-16">
     <div v-for="course in courses" :key="course.id">
       <img :src="course.imageUrl" class="w-full h-60 rounded object-cover" />
-
-
-
       <div class="mt-4">
         <div class="flex justify-between items-center">
           <div class="flex items-center">
             <span class="text-xs font-medium line-through text-gray-500">{{ course.price }}</span>
             <img src="/images/dot.png" class="w-4 mx-1" />
-            <span class="text-lg font-bold text-orange-600">{{ course.discountPrice }}</span>
+            <span class="text-base font-semibold text-orange-600">{{ course.discountPrice }}</span>
           </div>
           <router-link :to="{ name: 'courseDetail' }"><img src="/images/diagonal-arrow.png" class="w-4" /></router-link>
         </div>
-        <h1 class="text-xl font-bold mt-1">{{ course.title }}</h1>
+        <h1 class="text-lg font-semibold mt-1">{{ course.title }}</h1>
         <div class="badge bg-sky-200 text-sky-800 py-3 me-2 mb-1 mt-2">{{ course.tag }}</div>
       </div>
     </div>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,58 +1,77 @@
 <template>
-    <div class="container mx-auto px-8 2xl:px-40 pt-32">
-        <div class="grid gird-cols-1">
-        <div>
-            <h1 class="font-bold text-2xl capitalize text-center">Search among <span class="text-orange-400">58340</span> courses and<br/> find your favorite course</h1>
-          
-            <div class="mt-8 flex flex-col sm:flex-row sm:justify-between">
-              <select class="select select-bordered w-24 sm:w-28 md:w-56 hidden sm:block">
-                <option disabled selected>Filter</option>
-                <option>Sci-fi</option>
-                <option>Drama</option>
-                <option>Action</option>
-              </select>  
-
-              <label class="input input-bordered flex items-center gap-2 w-full md:w-3/5 order-1">
-                <input type="text" class="grow" placeholder="Search" />
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
-              </label>
-
-              <select class="select select-bordered w-full mt-2 sm:mt-0 sm:w-28 md:w-56 order-2">
-                <option disabled selected>Filter</option>
-                <option>Sci-fi</option>
-                <option>Drama</option>
-                <option>Action</option>
-              </select> 
-            </div>
-         
-            <div class="mt-8 flex justify-center">
-                <div class="flex flex-wrap">
-                    <a href="" class="border border-info bg-info text-white px-6 py-2 rounded-md me-5 mb-3">All Programme</a>
-
-                    <a href="" class="border border-gray-300 text-gray-400 px-6 py-2 rounded-md me-5 mb-3">UI/UX</a>
-
-                    <a href="" class="border border-gray-300 text-gray-400 px-6 py-2 rounded-md me-5 mb-3">PHP</a>
-
-                    <a href="" class="border border-gray-300 text-gray-400 px-6 py-2 rounded-md me-5 mb-3">Laravel</a>
-
-                    <a href="" class="border border-gray-300 text-gray-400 px-6 py-2 rounded-md me-5 mb-3">Vue</a>
-
-                    <a href="" class="border border-gray-300 text-gray-400 px-6 py-2 rounded-md me-5 mb-3">Inertia</a>
-                </div>
-            </div>  
-
-            <div class="mt-16 max-w-7xl">
-              <Course/>
-            </div>
+  <div class="container mx-auto px-8 2xl:px-40 pt-32">
+    <div class="grid gird-cols-1">
+      <div>
+        <h1 class="font-semibold text-2xl capitalize">Find your favorite courses !!</h1>
+        <!-- selectboxes  -->
+        <div class="mt-4 sm:flex gap-2 justify-end">
+          <select class="select select-bordered select-sm w-full sm:w-auto my-1">
+            <option disabled selected>Categories</option>
+            <option>Lorem ipsum dolor sit amet.</option>
+            <option>Drama</option>
+            <option>Action</option>
+          </select>
+          <select class="select select-bordered select-sm w-full sm:w-auto my-1">
+            <option disabled selected>Types</option>
+            <option>Sci-fi</option>
+            <option>Drama</option>
+            <option>Action</option>
+          </select>
+          <select class="select select-bordered select-sm w-full sm:w-auto my-1">
+            <option disabled selected>Paid or Free</option>
+            <option>Sci-fi</option>
+            <option>Drama</option>
+            <option>Action</option>
+          </select>
         </div>
+        <!-- search input  -->
+        <div class="flex flex-col sm:flex-row sm:justify-between mt-1">
+          <label class="input input-bordered flex items-center gap-2 w-full">
+            <input type="text" class="grow" placeholder="Search" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70">
+              <path fill-rule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clip-rule="evenodd" />
+            </svg>
+          </label>
+        </div>
+        <!-- tags -->
+        <div class="mt-4 flex justify-center">
+          <div class="flex flex-wrap gap-2">
+            <a href="" class="text-sm border border-info bg-info text-white py-1 px-3 rounded-lg">All Courses</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">UI/UX</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">PHP</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Laravel</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Vue</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Inertia</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">UI/UX</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">PHP</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Laravel</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Vue</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Inertia</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">UI/UX</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">PHP</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Laravel</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Vue</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Inertia</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">UI/UX</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">PHP</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Laravel</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Vue</a>
+            <a href="" class="text-sm border border-gray-300 text-gray-400 py-1 px-3 rounded-lg">Inertia</a>
+          </div>
+        </div>
+        <!-- courses  -->
+        <div class="mt-16 max-w-7xl">
+          <Course />
+        </div>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script setup>
 import Course from '../components/Course.vue'
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
