2002564cf6b4fd4a0cf859f295049ad795c98111
htetnandarbo <htetnandarbo8@gmail.com>
2024-06-12 10:07:23 +0630

    noti page modify

--- src/views/ProfileNotifications.vue
+++ src/views/ProfileNotifications.vue
@@ -34,7 +34,7 @@
             </div>
         </div>
 
-        <div class="flex bg-sky-10 ps-5 py-4 mb-1" onclick="my_modal_2.showModal()">
+        <div class="flex bg-sky-10 ps-5 py-4 mb-1">
             <div class="w-10">
                 <div class="bg-orange-50 w-10 h-10 rounded-full flex justify-center items-center">
                     <img src="/images/notification1.png" class="w-8 h-8" />
@@ -46,7 +46,16 @@
                         ipsum dolor sit
                         amet, consectetur adipisicing elit Lorem ipsum dolor sit netur esse neque lorem...</p>
                     <div class="basis-1/12 flex justify-end">
-                        <img src="/images/dot3.png" class="w-14 -mt-3.5" />
+                        <div class="dropdown dropdown-end">
+                            <div tabindex="0" role="button" class="m-1">
+                                <img src="/images/three-dots.png" class="w-6" />
+                            </div>
+                            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a onclick="my_modal_2.showModal()">view</a></li>
+                                <li><a>delete</a></li>
+                                <li><a>mark as read</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <span class="text-sm text-orange-600">1 hour ago</span>
@@ -65,7 +74,16 @@
                         ipsum dolor sit
                         amet, consectetur adipisicing elit Lorem ipsum dolor sit netur esse neque lorem...</p>
                     <div class="basis-1/12 flex justify-end">
-                        <img src="/images/dot3.png" class="w-14 -mt-3.5" />
+                        <div class="dropdown dropdown-end">
+                            <div tabindex="0" role="button" class="m-1">
+                                <img src="/images/three-dots.png" class="w-6" />
+                            </div>
+                            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a onclick="my_modal_2.showModal()">view</a></li>
+                                <li><a>delete</a></li>
+                                <li><a>mark as read</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <span class="text-sm text-orange-600">1 hour ago</span>
@@ -84,7 +102,16 @@
                         ipsum dolor sit
                         amet, consectetur adipisicing elit Lorem ipsum dolor sit netur esse neque lorem...</p>
                     <div class="basis-1/12 flex justify-end">
-                        <img src="/images/dot3.png" class="w-14 -mt-3.5" />
+                        <div class="dropdown dropdown-end">
+                            <div tabindex="0" role="button" class="m-1">
+                                <img src="/images/three-dots.png" class="w-6" />
+                            </div>
+                            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a onclick="my_modal_2.showModal()">view</a></li>
+                                <li><a>delete</a></li>
+                                <li><a>mark as read</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <span class="text-sm text-orange-600">1 hour ago</span>
@@ -103,13 +130,24 @@
                         ipsum dolor sit
                         amet, consectetur adipisicing elit Lorem ipsum dolor sit netur esse neque lorem...</p>
                     <div class="basis-1/12 flex justify-end">
-                        <img src="/images/dot3.png" class="w-14 -mt-3.5" />
+                        <div class="dropdown dropdown-end">
+                            <div tabindex="0" role="button" class="m-1">
+                                <img src="/images/three-dots.png" class="w-6" />
+                            </div>
+                            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a onclick="my_modal_2.showModal()">view</a></li>
+                                <li><a>delete</a></li>
+                                <li><a>mark as read</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <span class="text-sm text-orange-600">1 hour ago</span>
             </div>
         </div>
 
+
+        <!-- view modal -->
         <div>
             <dialog id="my_modal_2" class="modal">
                 <div class="modal-box md:w-9/12 w-11/12 max-w-5xl">
