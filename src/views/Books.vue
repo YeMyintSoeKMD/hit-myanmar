affa3e6330827439ce809631e00e10b4f56a2880
Htet Nandar <htetnandarbo8@gmail.com>
2024-07-02 11:07:51 +0630

    books update

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="container mx-auto px-8 2xl:px-40 pt-32">
-        <div class="grid grid-cols-2 flex items-center">
-            <div class="pe-24">
-                <!-- <h1 class="text-3xl tracking-wider"><span class="font-bold text-rose-500">H</span>appy <span class="font-bold text-teal-700">R</span>eading, </h1> -->
-                <h1 class="mint text-5xl tracking-widest">Happy Reading,</h1>
-                <h1 class="text-5xl tracking-wide mt-4 mint">Dar Dar</h1>
-                <p class="mt-8 leading-relaxed">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, nam,
+        <div class="grid lg:grid-cols-2 flex items-center">
+            <div class="xl:pe-24 lg:pe-5 lg:m-0 mt-12">
+                <h1 class="mint xl:text-5xl lg:text-4xl md:text-5xl sm:text-4xl text-3xl tracking-widest">Happy Reading,</h1>
+                <h1 class="xl:text-5xl lg:text-4xl md:text-5xl sm:text-4xl text-3xl tracking-wide mt-4 mint">Dar Dar</h1>
+                <p class="mt-8 leading-relaxed xl:text-base lg:text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, nam,
                     debitis quo sed neque eligendi hic, omnis optio est qui aut fugit quam. Eius voluptatum, minus
                     officia ut nesciunt temporibus?</p>
                 <button class="btn rounded-md bg-rose-500 text-white mt-6 flex items-center">
@@ -22,29 +21,26 @@
                     </svg>
                 </button>
             </div>
-            <div>
-                <img src="/images/bookhero.jpg" />
+            <div class="lg:order-last order-first">
+                <img src="/images/bookhero.jpg" class="lg:m-0 xl:w-full lg:w-11/12 md:w-3/5 w-11/12 mx-auto object-cover" />
             </div>
         </div>
 
-        <div class="mt-32">
+        <div class="pt-32">
+            <div class="grid xl:grid-cols-2 gap-2">
 
-
-
-            <div class="grid grid-cols-2 gap-2">
-
-                <div v-for="book in books" :key="book.id" class="grid grid-cols-5 card card-side bg-base-100 border">
-                    <figure class="col-span-2 pe-0.5">
-                        <img :src="book.imageUrl" alt="Book" class="h-full shadow-xl" />
+                <div v-for="book in books" :key="book.id" class="grid lg:grid-cols-5 md:grid-cols-3 card card-side bg-base-100 border">
+                    <figure class="xl:col-span-2 md:m-0 md:w-full md:shadow-none md:rounded-2xl md:rounded-tr-none md:rounded-br-none my-8 w-4/12 mx-auto shadow-lg rounded">
+                        <img :src="book.imageUrl" alt="Book" class="h-full md:border-r" />
                     </figure>
-                    <div class="col-span-3 card-body gap-0">
+                    <div class="lg:col-span-3 md:col-span-2 card-body gap-0">
                         <h2 class="card-title text-xl">{{ book.title }}</h2>
                         <h4 class="text-sm mt-1">By <span class="text-rose-500 font-semibold">{{ book.author }}</span>
                         </h4>
                         <p class="mt-5 leading-relaxed text-sm text-gray-700">{{ book.description }}</p>
                         <div class="mt-4 flex flex-wrap gap-2">
                             <div v-for="cat in book.category"
-                                class="badge badge-outline border-teal-500 rounded-md p-3 text-teal-800">{{ cat }}</div>
+                                class="badge bg-teal-100 rounded-md p-3 text-teal-700">{{ cat }}</div>
                         </div>
                         <!-- <p class="text-gray-500 mt-6 text-sm">Type : <span class="font-semibold text-rose-500">{{
                             book.type }}</span></p> -->
